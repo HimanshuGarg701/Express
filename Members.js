a631236331a3a9fdd48d024304e9ac754e526981
Himanshu Garg <hgarg701@gmail.com>
2020-08-30 22:48:56 -0700

    Initialised Model class Members

---
+++ Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id : 1,
+        name : 'Devil',
+        email : 'devil@mail.com'
+    },
+
+    {
+        id:2,
+        name : 'Angel',
+        email : 'angel.mail.com'
+    },
+
+    {
+        id:3,
+        name:'human',
+        email : 'human@mail.com'
+    }
+];
+
+module.exports(members);
+
